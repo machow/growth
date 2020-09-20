cb03b38a0e78f240fbc4467466eb96de2589b97d
Michael Chow <mc_al_github@fastmail.com>
2020-09-19 21:17:13 -0400

    use two curly braces templates

--- scripts/generate_webpage.py
+++ scripts/generate_webpage.py
@@ -1,6 +1,20 @@
 from pathlib import Path
+from string import Template
 
-template = Path("_template.html").read_text()
+class CustomTemplate(Template):
+    delimiter = '{{'
+    pattern = r'''
+    \{\{(?:
+    (?P<escaped>\{\{)|
+    (?P<named>[_a-z][_a-z0-9]*)\}\}|
+    (?P<braced>[_a-z][_a-z0-9]*)\}\}|
+    (?P<invalid>)
+    )
+    '''
+
+template = CustomTemplate(
+    Path("_template.html").read_text()
+    )
 
 content = "\n".join([p.read_text() for p in Path("posts").glob("*.md")])
 
